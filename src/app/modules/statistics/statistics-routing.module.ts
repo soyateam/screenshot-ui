37840d809e1924d5d618643332beba0f83e2be85
Sraya Avraham <sraya305@gmail.com>
2020-08-20 09:54:07 +0300

    feat: statistics component for task

---
+++ src/app/modules/statistics/statistics-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TaskStatisticsComponent } from './components/task-statistics/task-statistics.component';
+
+const routes: Routes = [ { path: 'statistics/:id', component: TaskStatisticsComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class StatisticsRoutingModule { }
--- src/app/modules/statistics/statistics.module.ts
+++ src/app/modules/statistics/statistics.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { GraphComponent } from './components/graph/graph.component';
+import { TaskStatisticsComponent } from './components/task-statistics/task-statistics.component';
+import { StatisticsRoutingModule } from './statistics-routing.module';
 
 
 @NgModule({
-    declarations: [GraphComponent],
+    declarations: [GraphComponent, TaskStatisticsComponent],
     imports: [
         CommonModule,
         SharedModule,
+        StatisticsRoutingModule
     ],
     exports: [GraphComponent]
 })
