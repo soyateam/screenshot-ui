6a93acf97a21d0265d101aeb6c4c3738009fd62e
nogahaskal <noghas12@gmail.com>
2020-08-25 10:42:56 +0300

    fix: tslint

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { LoaderInterceptor } from './intercptors/loader.iterceptor';
 import { AuthInterceptor } from './intercptors/auth.iterceptor';
